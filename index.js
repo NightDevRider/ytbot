71b341fa676c910ee12e0bbeb0f8e34a22d03e74
NightDevRider <44276814+NightDevRider@users.noreply.github.com>
2018-10-20 14:38:06 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,101 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const ytdl = require('ytdl-core');
+const request = require("request");
+const fs = require("fs");
+const getYouTubeID = require("get-youtube-id");
+const fetchVideoInfo = require("youtube-info");
+
+var config = JSON.parse(fs.readFileSync('./settings.json', 'utf-8'));
+
+const yt_api_key = config.yt_api_key;
+const bot_controller = config.bot_controller;
+const prefix = config.prefix;
+const discord_token = config.discord_token;
+
+var queue = [];
+var isPlaying = false;
+var dispatcher = null;
+var voiceChannel = null;
+var skipReq = 0;
+var skippers = [];
+
+client.login(discord_token);
+
+client.on('ready', function () {
+  const member = message.member;
+  const mess = message.content.toLowerCase();
+  const args = message.content.split(' ').slice(1).join(" ");
+
+  if (mess.startsWith(prefix + "play")) {
+    if (queue.lenght > 0 || isPlaying) {
+      getID(args, function (id) {
+        add_to_queue(id);
+        fetchVideoInfo(id, function (err, videoInfo){
+          if (err) throw new Error(err);
+          message.reply(" added in queue: **" + videoInfo.title + "**");
+        });
+      });
+    } else {
+      ifPlaying = true;
+      getID(args, function (id) {
+        queue.push("placeholder");
+        playMusic(id, message);
+        fetchVideoInfo(id, function (err, videoInfo){
+          if (err) throw new Error(err);
+          message.reply(" now playing: **" + videoInfo.title + "**");
+        });
+      });
+    }
+  }
+
+});
+
+
+
+client.on('ready', function () {
+  console.log("I am ready")
+});
+
+function playMusic(id, message) {
+  voiceChannel = message.member.voiceChannel;
+
+  voiceChannel.join().then(function (connection) {
+    stream = ytdl("https://www.youtube.com/watch?v=" + id, {
+      filter: 'audioonly'
+    });
+    skipReq = 0;
+    skippers = [];
+
+    dispatcher = connection.playStream(stream);
+  });
+}
+
+function getID(str, cb) {
+  if (isYoutube(str)) {
+    cb(getYouTubeID(str));
+  } else {
+    search_video(str, function(id) {
+      cb(id);
+    });
+  }
+}
+
+function add_to_queue(strID) {
+  if (isYoutube(strID)) {
+    queue.push(getYouTubeID(strID));
+  } else {
+    queu.push(strID);
+  }
+}
+
+function search_video(query, callback) {
+  request("https://www.googleapis.com/youtube/v3/search?part=id&type=video&q=" + encodeURLComponent(query) + "&key=" + yt_api_key, function(error, response, body) {
+    var json = JSON.parse(body);
+    callback(json.items[0].id.videoId);
+  });
+}
+
+function isYoutube(str) {
+  return str.toLowerCase().indexOf("youtube.com") > -1;
+}
